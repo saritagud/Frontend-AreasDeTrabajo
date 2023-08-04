eeb76d7344d9604dca2304cf2dc7e2cff7d8f208
José Francisco Uzcátegui González <100871295+Fran-JF@users.noreply.github.com>
2023-08-04 17:59:21 -0400

    Actualización usuario sin reservas

--- Frontend/src/Components/PerfilUsuario.jsx
+++ Frontend/src/Components/PerfilUsuario.jsx
@@ -4,7 +4,7 @@ import { selectUser } from "../features/auth/authSlice";
 import { useTranslation } from "react-i18next";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { getBookingsByUser } from "../api/bookingsApi"; 
+import { getBookingsByUser } from "../api/bookingsApi";
 import { getUserBookingsSuccess, selectUserBookings } from "../features/bookings/bookingsSlice";
 
 export default function PerfilUsuario() {
@@ -12,12 +12,12 @@ export default function PerfilUsuario() {
   const bookings = useSelector(selectUserBookings); // Usa el selector para obtener las reservas del usuario desde el estado de Redux
   const dispatch = useDispatch();
 
+  const { t, i18n } = useTranslation();
+
   useEffect(() => {
     // Obtiene las reservas del usuario al montar el componente
     getBookingsByUser(user.id).then((res) => dispatch(getUserBookingsSuccess(res)));
   }, [dispatch, user.id]);
-
-  const { t, i18n } = useTranslation();
 
   return (
     <>
@@ -35,7 +35,8 @@ export default function PerfilUsuario() {
         <h2 className="wp-full xl:w-[70%] xl:text-left">{t("reservation")}</h2>
 
         <div className="grid grid-cols-3 gap-4">
-          {bookings.map((booking) => (
+
+          {bookings.length > 0 ? bookings.map((booking) => (
             // iteramos sobre el arreglo de reservas y muestra la información que desees
             <div key={booking._id} className="bg-azulClaro p-4 rounded-lg shadow-md my-4 border-2 border-azulOscuro border-opacity-50 max-w-xs">
               <p className="text-right text-sm">{booking.precioTotal}</p>
@@ -48,7 +49,11 @@ export default function PerfilUsuario() {
               <p className="text-sm font-bold">Hora:</p> {/* Agrega la clase font-bold */}
               <p className="text-sm">{new Date(booking.horaInicioYFinal.horaInicio).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} - {new Date(booking.horaInicioYFinal.horaFin).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p> {/* Usa toLocaleTimeString para formatear las horas */}
             </div>
-          ))}
+          )) :
+            <div className="text-center text-2xl text-gray-600 italic">
+              No hay reservaciones
+            </div>
+          }
         </div>
       </section>
       <Footer />
